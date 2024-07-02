55a11dc6629ccb22ea8e06e91bcb3459c9881f38
azkia ajmal fairuz <azkiajmal@gmail.com>
2024-07-02 22:50:29 +0700

    feat: anwer the question

--- app/src/main/java/com/example/catcareexpertsystem/diagnose/DiagnoseViewmodel.kt
+++ app/src/main/java/com/example/catcareexpertsystem/diagnose/DiagnoseViewmodel.kt
@@ -1,10 +1,12 @@
 package com.example.catcareexpertsystem.diagnose
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.catcareexpertsystem.penyakit.Penyakit
 import com.example.catcareexpertsystem.penyakit.PenyakitRepository
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 
 class DiagnoseViewmodel : ViewModel() {
     private val repository = PenyakitRepository()
@@ -14,17 +16,25 @@ class DiagnoseViewmodel : ViewModel() {
     private val _currentQuestionIndex = MutableStateFlow(0)
     val currentQuestionIndex: StateFlow<Int> = _currentQuestionIndex
 
+    init {
+        fetchQuestion()
+    }
+
     private val _answer = MutableStateFlow<List<Answer>>(emptyList())
     val answer: StateFlow<List<Answer>> = _answer
 
-    suspend fun fetchQuestion() {
-        val result = repository.getData()
-        _quetion.value = result
+    private fun fetchQuestion() {
+        viewModelScope.launch {
+            val result = repository.getData()
+            _quetion.value = result
+        }
     }
 
     fun answerQuestion(questionCode: String, cf: Double) {
         _answer.value += Answer(questionCode, cf)
-        if(_currentQuestionIndex.value < _quetion.value.size -1){
+        if (_currentQuestionIndex.value < _quetion.value.size - 1) {
+            _currentQuestionIndex.value++
+        }else{
             _currentQuestionIndex.value++
         }
     }
--- app/src/main/java/com/example/catcareexpertsystem/diagnose/QuestionScreen.kt
+++ app/src/main/java/com/example/catcareexpertsystem/diagnose/QuestionScreen.kt
@@ -1,9 +1,13 @@
 package com.example.catcareexpertsystem.diagnose
 
+import android.util.Log
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -12,15 +16,61 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.catcareexpertsystem.penyakit.Penyakit
+import com.example.catcareexpertsystem.ui.theme.CatcareexpertsystemTheme
+import com.example.catcareexpertsystem.ui.theme.Primary
 
 @Composable
 fun QuestionScreen() {
+    val viewmodel: DiagnoseViewmodel = viewModel()
+    val question = viewmodel.quetion.collectAsState().value
+    val currentQuestionIndex = viewmodel.currentQuestionIndex.collectAsState().value
+    val currentQuestion = question.getOrNull(currentQuestionIndex)
+
+    val answers = viewmodel.answer.collectAsState().value
+
+    Box(modifier = Modifier
+        .fillMaxSize()
+        .background(Primary)
+        .padding(16.dp)
+    ) {
+        currentQuestion?.let {
+            QuestionCard(
+                question = it,
+                onYesClick = { viewmodel.answerQuestion(it.penyakitCode, 0.6) },
+                onNoClick = { viewmodel.answerQuestion(it.penyakitCode, cf = 0.4) }
+            )
+        }
+        if (currentQuestionIndex  > question.size -1 ) {
+
+                AnswerHistory(answers = answers)
+        }
+    }
 
 }
-
+@Composable
+fun AnswerHistory(answers: List<Answer>) {
+    Column {
+        Text(
+            text = "Riwayat Jawaban:",
+            style = MaterialTheme.typography.headlineSmall,
+            modifier = Modifier.padding(bottom = 16.dp)
+        )
+        answers.forEach { answer ->
+            Text(
+                text = "Pertanyaan: ${answer.penyakitCode}, Nilai: ${answer.cf}",
+                style = MaterialTheme.typography.bodyLarge,
+                modifier = Modifier.padding(bottom = 8.dp)
+            )
+        }
+    }
+}
 @Composable
 fun QuestionCard(question: Penyakit, onYesClick: () -> Unit, onNoClick: () -> Unit) {
     Card(
@@ -28,7 +78,7 @@ fun QuestionCard(question: Penyakit, onYesClick: () -> Unit, onNoClick: () -> Un
             .fillMaxWidth()
             .padding(vertical = 8.dp),
 
-    ) {
+        ) {
         Column(modifier = Modifier.padding(16.dp)) {
             Text(text = question.penyakit, style = MaterialTheme.typography.headlineSmall)
             Spacer(modifier = Modifier.height(8.dp))
@@ -44,5 +94,14 @@ fun QuestionCard(question: Penyakit, onYesClick: () -> Unit, onNoClick: () -> Un
                 }
             }
         }
+    }
+}
+
+
+@Preview()
+@Composable
+fun PreviewQuestionView() {
+    CatcareexpertsystemTheme {
+        QuestionScreen()
     }
 }
