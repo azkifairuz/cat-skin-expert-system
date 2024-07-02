676e3cf59fa5f58ddadac64b99b0e69f2d9b2e77
azkia ajmal fairuz <azkiajmal@gmail.com>
2024-07-02 22:15:35 +0700

    feat:question card

---
+++ app/src/main/java/com/example/catcareexpertsystem/diagnose/QuestionScreen.kt
@@ -0,0 +1,48 @@
+package com.example.catcareexpertsystem.diagnose
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.catcareexpertsystem.penyakit.Penyakit
+
+@Composable
+fun QuestionScreen() {
+
+}
+
+@Composable
+fun QuestionCard(question: Penyakit, onYesClick: () -> Unit, onNoClick: () -> Unit) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 8.dp),
+
+    ) {
+        Column(modifier = Modifier.padding(16.dp)) {
+            Text(text = question.penyakit, style = MaterialTheme.typography.headlineSmall)
+            Spacer(modifier = Modifier.height(8.dp))
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Button(onClick = onYesClick) {
+                    Text(text = "Yes (0.6)")
+                }
+                Button(onClick = onNoClick) {
+                    Text(text = "No (0.4)")
+                }
+            }
+        }
+    }
+}
