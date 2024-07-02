b349d06a9732eb9316189600dc3d0c9f62038091
azkia ajmal fairuz <azkiajmal@gmail.com>
2024-07-02 22:12:43 +0700

    feat: answer question function

--- app/src/main/java/com/example/catcareexpertsystem/diagnose/DiagnoseViewmodel.kt
+++ app/src/main/java/com/example/catcareexpertsystem/diagnose/DiagnoseViewmodel.kt
@@ -6,7 +6,7 @@ import com.example.catcareexpertsystem.penyakit.PenyakitRepository
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 
-class DiagnoseViewmodel: ViewModel() {
+class DiagnoseViewmodel : ViewModel() {
     private val repository = PenyakitRepository()
     private val _quetion = MutableStateFlow<List<Penyakit>>(emptyList())
     val quetion: StateFlow<List<Penyakit>> = _quetion
@@ -15,10 +15,17 @@ class DiagnoseViewmodel: ViewModel() {
     val currentQuestionIndex: StateFlow<Int> = _currentQuestionIndex
 
     private val _answer = MutableStateFlow<List<Answer>>(emptyList())
-    val answer:StateFlow<List<Answer>> = _answer
+    val answer: StateFlow<List<Answer>> = _answer
 
     suspend fun fetchQuestion() {
         val result = repository.getData()
         _quetion.value = result
+    }
+
+    fun answerQuestion(questionCode: String, cf: Double) {
+        _answer.value += Answer(questionCode, cf)
+        if(_currentQuestionIndex.value < _quetion.value.size -1){
+            _currentQuestionIndex.value++
+        }
     }
 }
